4ba4e5082258dd767ed81c830a091bacc29120b2
antoine.combet <antoine.combet@TLPU151.ETAB.LOCAL>
2019-03-28 15:58:38 +0100

    Added a bunch of prototypes for future functions for img db

--- image_database.py
+++ image_database.py
@@ -10,11 +10,30 @@ def constantSetup(sDataBasePath):
 
 def tableSetup():
     with sql.connect(DB_PATH) as db:
-        db.execute("""CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+        db.execute("""CREATE TABLE images (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
                                           title TEXT,
                                           description TEXT,
+                                          tags TEXT,
                                           publisher INTEGER,
                                           file_path TEXT,
                                           publish_date DATE)""")
         db.commit()
         db.close()
+
+def getImagePath(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT file_path FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
+
+def getImageTitle(nId): pass
+
+def getImageTags(nId): pass
+
+def getImageOwner(nId): pass
+
+def getImageDesc(nId): pass
+
+def getImageDate(nId): pass
+
+def getImageInfo(nId): pass
