1ade654564e1e76c6f676a1fd7dacb32f8898a10
Annwan <30665559+Annwan@users.noreply.github.com>
2019-03-29 13:48:36 +0100

    Add files via upload

--- image_database.py
+++ image_database.py
@@ -16,7 +16,7 @@ def tableSetup():
                                           tags TEXT,
                                           publisher INTEGER,
                                           file_path TEXT,
-                                          publish_date DATE)""")
+                                          publish_time INT)""")
         db.commit()
         db.close()
 
@@ -26,14 +26,56 @@ def getImagePath(nId):
         cur.execute("SELECT file_path FROM images WHERE id=?",(nId,))
         for info in cur: return info[0]
 
-def getImageTitle(nId): pass
+def getImageTitle(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT title FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
 
-def getImageTags(nId): pass
+def getImageTags(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT tags FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
 
-def getImageOwner(nId): pass
+def getImageOwner(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT publisher FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
 
-def getImageDesc(nId): pass
+def getImageDesc(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT description FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
 
-def getImageDate(nId): pass
+def getImageTime(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT publish_time FROM images WHERE id=?",(nId,))
+        for info in cur: return info[0]
 
-def getImageInfo(nId): pass
+def getImageInfo(nId):
+    with sql.connect(DB_PATH) as db:
+        cur = db.cursor()
+        cur.execute("SELECT id, title, description, tags, publisher, file_path, publish_time FROM images WHERE id=?",(nId,))
+        for info in cur:
+            return {
+                "id" : info[0],
+                "title": info[1],
+                "description": info[2],
+                "tags": info[3],
+                "publisher": info[4],
+                "file_path": info[5],
+                "publish_time": info[6]
+            }
+
+def addImage(sPath, sTitle, nOwner, sDescription, sTags):
+    nTime = int(time.time())
+    with sql.connect(DB_PATH) as db:
+        db.execute("INSERT INTO images(title, description, tags, publisher, file_path, publish_time) VALUES (?,?,?,?,?,?)",
+            (sTitle, sDescription, sTags, nOwner, sPath, nTime))
+        db.commit()
+        db.close()
+
