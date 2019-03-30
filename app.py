a2c14a37669b5293bd761eaa7c187a22be77ff97
Annwan <anwankong@gmail.com>
2019-03-30 21:59:25 +0100

    .

---
+++ app.py
@@ -0,0 +1,10 @@
+import cherrypy as cp
+
+import user_database as users
+import image_database as images
+
+class Webapp(object):
+    """docstring for Webapp."""
+    @cp.expose()
+    def index(self):
+        return open("page1.html")
---
+++ main.py
@@ -0,0 +1,1 @@
+import cherrypy as cp
