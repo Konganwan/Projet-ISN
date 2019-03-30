b8bf242f88d0072579a3e5728e5c7a7a4e759d8a
Annwan <anwankong@gmail.com>
2019-03-30 22:02:16 +0100

    .

--- setublib.sh
+++ setublib.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if [$# -eq 2] then
-  echo "installation des bibliotheques"
-  if [$3 = "3"] then
-    exec "pip3 install pip CherryPy --upgrade"
-  else
-    exec "pip install pip CherryPy --upgrade"
-  fi
-else
-  return true
-fi
+
+echo "installation des bibliotheques"
+exec `pip3 install pip CherryPy --upgrade`
+echo "fait"
