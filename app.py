46885f17a5abf6a4af832e2bef9f2072c28d8178
Annwan <anwankong@gmail.com>
2019-04-04 21:10:44 +0200

    .

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ class Webapp(object):
     @cp.expose()
     def index(self):
         return open("page1.html")
+
+    @cp.expose()
+    def show_image(self, iid):
+        page = open("show_image.html")
+            page.read()
+
+    @cp.expose
+    def login(self):
+      return open("login.html")
+
+    @cp.expose
+    def signup(self):
+      return open("signup.html")
+
+    @cp.expose
+    def login_status(self,mail,pwd):
+      return open("login_status.html")
+     
