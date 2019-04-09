2c751a2493e2a3df071cb0121cd78a111f1213ce
Annwan <anwankong@gmail.com>
2019-04-09 10:41:27 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import cherrypy as cp
 
 from app import Webapp
-
+conf ={
+    '/':{},
+    '/static':{
+        'tools.staticdir.on':True,
+        'tools.staticdir.dir':'./static'
+    }
+}
 cp.quickstart(Webapp())
