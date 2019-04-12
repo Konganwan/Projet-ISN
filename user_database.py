a676b6b684fda53ecdf62506242f3bc60b393c74
Annwan <anwankong@gmail.com>
2019-04-12 13:20:27 +0200

    .

--- user_database.py
+++ user_database.py
@@ -20,18 +20,20 @@ def tableSetup():
 
 
 def addUser(sName, sPwd, sMail):
-    """Adds a user in the database"""
+    """Adds an user into the database"""
     with sql.connect(DB_PATH) as db:
         sHashed=hasher.sha256(codecs.encode(sPwd)).hexdigest()
         db.execute("INSERT INTO users(name,pwd_hash,mail) VALUES (?,?,?)",(sName,sHashed,sMail))
         db.commit()
 
 def removeUser(nUserID):
+    """Delete an users from the database"""
     with sql.connect(DB_PATH) as db:
-        db.execute("REMOVE FROM users WHERE id=?",(nUserID,))
+        db.execute("DELETE FROM users WHERE id=?",(nUserID,))
         db.commit()
 
 def checkUserPassword(sGivenPwd, sMail):
+    """Checks if a given password matches with the one of a given user"""
     sGPwdHash = hasher.sha256(codecs.encode(sGivenPwd)).hexdigest()
     sStoredHash = ""
     with sql.connect(DB_PATH) as db:
@@ -40,36 +42,37 @@ def checkUserPassword(sGivenPwd, sMail):
         for info in cur:
             sStoredHash = info[0]
         cur.close()
-        if sStoredHash == sGPwdHash:
-            return True
-        else:
-            return False
+    return sStoredHash == sGPwdHash
 
 def chekUserExists(sMail):
+    """Checks if a given e-mail adress is the one of a registered user"""
+    nId = None
     with sql.connect(DB_PATH) as db:
         cur = db.cursor()
         cur.execute("SELECT id FROM users WHERE  mail=?",(sMail,))
-        nId = None
         for info in cur:
             nId = info[0]
         cur.close()
-        if nId is not None:
-            return True
-        else:
-            return False
+    return nId is not None
 
 def getUserByMail(sMail):
+    """Gets all available info about the user corresponding to the given mail"""
+    info = [None]*4
     with sql.connect(DB_PATH) as db:
         cur = db.cursor()
         cur.execute("SELECT * FROM users WHERE  mail=?",(sMail,))
-        for info in cur:
-            return info
+        for i in cur:
+            info = i
         cur.close()
+    return info
 
 def getUserById(nId):
+    """Gets all available info about the user corresponding to the given id"""
+    info = [None]*4
     with sql.connect(DB_PATH) as db:
         cur = db.cursor()
         cur.execute("SELECT * FROM users WHERE  id=?",(nId,))
-        for info in cur:
-            return info
+        for i in cur:
+            info = i
         cur.close()
+    return info
