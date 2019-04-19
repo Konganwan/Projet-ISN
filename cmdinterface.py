75fac380d860c5ff7a417900c935c72b85387070
Annwan <anwankong@gmail.com>
2019-04-19 09:58:57 +0200

    .

--- cmdinterface.py
+++ cmdinterface.py
@@ -19,7 +19,7 @@ class CLI(threading.Thread):
                     args = inp[2:]
                     users.addUser(args[0],args[1],args[2])
                 elif inp[1].lower() in ["getmail","gm"]:
-                    ulist = users.getUserByMail(inp[2])
+                    ulist = users.getUserByMail("*"+inp[2]+"*")
                     for data in ulist:
                         for field in data.keys():
                             print(field,":",data[field])
@@ -37,7 +37,7 @@ class CLI(threading.Thread):
                     else:
                         print(f'User n°{arg} removed')
                 elif inp[1].lower() in ["gn","getname"]:
-                    ulist = users.getUserByName(inp[2])
+                    ulist = users.getUserByName("*"+inp[2]+"*")
                     for data in ulist:
                         for field in data.keys():
                             print(field,":",data[field])
@@ -51,7 +51,7 @@ class CLI(threading.Thread):
                             for field in data.keys():
                                 print(field,":",data[field])
                 elif inp[1].lower() in ["gt","gettitle"]:
-                    ilist = images.getImageByTitle(inp[2])
+                    ilist = images.getImageByTitle("*"+inp[2]+"*")
                     for data in ilist:
                         if type(data) is dict:
                             for field in data.keys():
