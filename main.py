58c72bd5d8dbe839a627b5699cb21ff1894ffdd8
Annwan <anwankong@gmail.com>
2019-05-07 11:31:30 +0200

    .

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3.6
+# -*- encoding: utf-8 -*-
+
+# ---------------------------
+# Import de librairies exterieures
+# ---------------------------
 import cherrypy as cp
 import os
+
+# ---------------------------
+# Import de fichiers locaux
+# ---------------------------
+
 from app import Webapp
 from cmdinterface import CLI
+
+# ---------------------------
+# Configuration du serveur web
+# ---------------------------
+
 conf ={
+    # Paramètres généraux
     '/':{
+        # Utilisation des outils de sessions utilisateurs
         'tools.sessions.on':True,
+
+        # Tous les chemins d'accès des fichiers sont exprimés par raport au dossier du programme
         'tools.staticdir.root': os.path.abspath(os.getcwd())
     },
     '/static':{
@@ -13,9 +32,10 @@ conf ={
         'tools.staticdir.dir':'./static'
     }
 }
-cp.config.update({'log.screen': False,
-                        'log.access_file': 'access.log',
-                        'log.error_file': 'error.log'})
+# Config de Logging
+cp.config.update({'log.screen': False, # Disable onscreen logging to make room²
+                  'log.access_file': 'access.log',
+                  'log.error_file': 'error.log'})
 
 cli = CLI(cp.engine)
 cli.start()
